192d8fc09e32f08842a1d8dfe7177377ecd068e3
deanben87 <az.benamara@gmail.com>
2022-05-31 20:35:38 -0500

    1st commit

---
+++ src/gitReview/gitReview01.java
@@ -0,0 +1,14 @@
+package gitReview;
+
+public class gitReview01 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("todya we learn git");
+		System.out.println("today is a very good day");
+		
+
+	}
+
+}
